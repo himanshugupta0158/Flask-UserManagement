04511c49a10157c23bf70a037cad3baa725886e0
himanshugupta0158 <himanshugupta0158@gmail.com>
2021-12-07 19:10:49 +0530

    added new ffunctions

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+# importing flask_bcrypt for hashing password and checking it
+from flask_bcrypt import Bcrypt
+# for managing login 
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -12,7 +15,21 @@ app.config['SECRET_KEY'] = '5776ca5151016c5a9198bb1d95a09e0e'
 
 # connecting to sqlalchemy db
 
+# below is db connection path
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# below is db variable for using db operation and creating tables in db.
 db = SQLAlchemy(app)
+
+# bcrypt hash password variable
+bcrypt = Bcrypt(app)
+
+# LoginManager variable
+login_manager = LoginManager(app)
+
+# setting login view here
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
 
 from app import routes
