bebdec3656df748317b3fe840165941f79c1af41
himanshugupta0158 <himanshugupta0158@gmail.com>
2021-12-08 19:22:31 +0530

    email password validation bug need to solve

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask_bcrypt import Bcrypt
 # for managing login 
 from flask_login import LoginManager
 from os.path import join, dirname, realpath
+from flask_mail import Mail
+import os
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5776ca5151016c5a9198bb1d95a09e0e'
@@ -30,6 +33,15 @@ login_manager = LoginManager(app)
 # setting login view here
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
+mail = Mail(app)
+
 
 
 
